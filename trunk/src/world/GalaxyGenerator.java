54412514ecb44a6396db62934fc20cd1792c685e
betelgeuze <betelgeuze@031dce99-5058-470c-816d-f0d31102f6e2>
2010-09-20 16:18:01 +0000

    Color the stars.

--- trunk/src/world/GalaxyGenerator.java
+++ trunk/src/world/GalaxyGenerator.java
@@ -1,5 +1,6 @@
 package world;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.Random;
 
 import org.lwjgl.BufferUtils;
 
+import lw3d.math.Noise;
 import lw3d.renderer.Geometry;
 import lw3d.renderer.Geometry.Attribute;
 import lw3d.renderer.Geometry.PrimitiveType;
@@ -17,34 +19,66 @@ import lw3d.renderer.Geometry.Type;
 public class GalaxyGenerator {
 	public static Geometry generateGalaxyPointGeometry(long seed) {
 		
-		double[] noiseSeedR = new double[16*16*16];
-		double[] noiseSeedI = new double[16*16*16];
-		
 		Random rand = new Random(seed);
+		
+		/*double[] noiseSeedR = new double[16*16*16];
+		double[] noiseSeedI = new double[16*16*16];
 		
 		for(int i = 0; i < noiseSeedR.length; i++) {
 			noiseSeedR[i] = 2.0 * rand.nextDouble() - 1.0;
 			noiseSeedI[i] = 2.0 * rand.nextDouble() - 1.0;
-		}
+		}*/
+		
+		
 		
 		List<Float> starPositions = new ArrayList<Float>();
+		List<Byte> color = new ArrayList<Byte>();
+		
+		
+		Noise noiseSeedR = new Noise(seed),
+			noiseSeedI = new Noise(seed ^42l);
+		
+		int subdivs = 50;
+		int starsTotal = 100;
 		
 		// Go through all sectors
-		for(int x = 0; x < 16; x++) {
-			for(int y = 0; y < 16; y++) {
-				for(int z = 0; z < 16; z++) {
+		for(float x = 0.5f; x < subdivs; x++) {
+			for(float y = 0.5f; y < subdivs; y++) {
+				for(float z = 0.5f; z < subdivs; z++) {
 					
 					// Amount of stars in this sector
-					int amount = (int) ( 100.0 * Math.sin(x
-							+ noise(noiseSeedR, noiseSeedI, x, y, z) 
-							+ 0.5*noise(noiseSeedR, noiseSeedI, 2*x, 2*y, 2*z) 
-							+ 0.25*noise(noiseSeedR, noiseSeedI, 4*x, 4*y, 4*z)
-							+ 0.125*noise(noiseSeedR, noiseSeedI, 8*x, 8*y, 8*z)) );
+					/*int amount = (int) ( (float)starsTotal/subdivs *Math.sin( x/Math.PI + 
+							noise(noiseSeedR, noiseSeedI, x/subdivs, y/subdivs, z/subdivs) 
+							+ 0.5*noise(noiseSeedR, noiseSeedI, 2*x/subdivs, 2*y/subdivs, 2*z/subdivs) 
+							+ 0.25*noise(noiseSeedR, noiseSeedI, 4*x/subdivs, 4*y/subdivs, 4*z/subdivs)
+							+ 0.125*noise(noiseSeedR, noiseSeedI, 8*x/subdivs, 8*y/subdivs, 8*z/subdivs)) );*/
+					
+					int amount = 1;
 					for(int i = 0; i < amount; i++) {
 						// Put a star in the sector
-						starPositions.add(((float)x + rand.nextFloat())/16f - 0.5f);
-						starPositions.add(((float)y + rand.nextFloat())/16f - 0.5f);
-						starPositions.add(((float)z + rand.nextFloat())/16f - 0.5f);
+						starPositions.add(((float)x + rand.nextFloat())/subdivs - 0.5f);
+						starPositions.add(((float)y + rand.nextFloat())/subdivs - 0.5f);
+						starPositions.add(((float)z + rand.nextFloat())/subdivs - 0.5f);
+						
+						// TODO: Improve colors
+						
+						float c = rand.nextFloat();
+						
+						if(c < 0.1f) {
+							color.add((byte)255);
+							color.add((byte)(210-(c-1f)));
+							color.add((byte)(170-(c-1f)));
+						}
+						else if( c < 0.9f) {
+							color.add((byte)255);
+							color.add((byte)255);
+							color.add((byte)255);
+						}
+						else {
+							color.add((byte)(190*c*c));
+							color.add((byte)(210*c));
+							color.add((byte)255);
+						}
 					}	
 				}
 			}
@@ -61,8 +95,22 @@ public class GalaxyGenerator {
 		while (fIt.hasNext())
 			((FloatBuffer) at.buffer).put(fIt.next());
 		at.buffer.flip();
-
+		
 		attributes.add(at);
+		
+		Attribute atColor = new Attribute();
+		atColor.buffer = BufferUtils.createByteBuffer(color.size());
+		atColor.name = "color";
+		atColor.normalized = true;
+		atColor.size = 3;
+		atColor.type = Type.BYTE;
+
+		Iterator<Byte> fItC = color.iterator();
+		while (fItC.hasNext())
+			((ByteBuffer) atColor.buffer).put(fItC.next());
+		atColor.buffer.flip();
+
+		attributes.add(atColor);
 		
 		// TODO: Are the indices needed?
 		int amountOfStars = starPositions.size() / 3;
@@ -75,15 +123,19 @@ public class GalaxyGenerator {
 		return new Geometry(PrimitiveType.POINTS, indices, attributes);
 	}
 	
-	private static double noise(double[] noiseSeedR, double[] noiseSeedI, int x, int y, int z) {
+	private static double noise(Noise noiseSeedR, Noise noiseSeedI, float x, float y, float z) {
 		// Modulate the coordinate into the sectors
-		x %= 256;
-		y %= 256;
-		z %= 256;
+		x %= 16;
+		y %= 16;
+		z %= 16;
 		
-		int indexH = (x % 16) + 16 * (y % 16) + 256 * (z % 16);
-		int indexL = (int) (Math.floor((double) x / 16) + 16 * Math.floor((double) y / 16) + 256 * Math.floor((double) y / 16));
-		double phase = Math.PI * noiseSeedR[indexL];
-		return noiseSeedR[indexH] * Math.cos(phase) + noiseSeedI[indexL] * Math.sin(phase);
+		/*x += 0.11;
+		y += 0.11;
+		z += 0.11;*/
+		
+		
+
+		double phase = Math.PI * noiseSeedR.noise(9*x / 8.0, 9*y / 8.0, 9*z / 8.0);
+		return noiseSeedR.noise(9*x,9*y,9*z) * Math.cos(phase) + noiseSeedI.noise(9*x,9*y,9*z) * Math.sin(phase);
 	}
 }
